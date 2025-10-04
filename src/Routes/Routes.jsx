84f5c7e800a7eccf7a7220c9cdbdad7c51a76c7f
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-04 21:38:42 +0600

    navbar ui

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,7 +1,18 @@
 import { createBrowserRouter } from "react-router";
+import Root from "../pages/Root/Root";
+import ErrorPage from "../pages/ErrorPage/ErrorPage";
+import Home from "../pages/Home/Home";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        path:"/",
+        Component: Home
+      }
+    ]
   },
 ]);
