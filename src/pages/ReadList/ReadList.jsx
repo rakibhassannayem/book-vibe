535951a973213bbe98c770c40f11098a3a883b6b
rakibhassannayem <rakibhassannayem@gmail.com>
2025-10-05 21:10:42 +0600

    readlist ui

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -4,6 +4,7 @@ import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { getStoredBook } from "../../utility/addToLS";
 import Book from "../Book/Book";
+import { ChevronDown } from "lucide-react";
 
 const ReadList = () => {
   const [readList, setReadList] = useState([]);
@@ -16,6 +17,28 @@ const ReadList = () => {
   }, []);
   return (
     <div>
+      <div className="flex justify-center">
+        <details className="dropdown">
+          <summary
+            className="btn m-1 bg-primary text-wrokSans text-lg rounded-lg
+          "
+          >
+            Sort By
+            <ChevronDown />
+          </summary>
+          <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li>
+              <a>Rating</a>
+            </li>
+            <li>
+              <a>Number of pages</a>
+            </li>
+            <li>
+              <a>Publishing Year</a>
+            </li>
+          </ul>
+        </details>
+      </div>
       <Tabs>
         <TabList>
           <Tab>Read Books</Tab>
@@ -24,9 +47,11 @@ const ReadList = () => {
 
         <TabPanel>
           <h2>Book i read ({readList.length})</h2>
-          {readList.map((singleBook) => (
-            <Book key={singleBook.bookId} singleBook={singleBook}></Book>
-          ))}
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mb-5">
+            {readList.map((singleBook) => (
+              <Book key={singleBook.bookId} singleBook={singleBook}></Book>
+            ))}
+          </div>
         </TabPanel>
         <TabPanel>
           <h2>My wishlist </h2>
